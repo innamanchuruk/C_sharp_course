20d6955cdb4bb1df9c3aff46ae9ddb283ae36f3b
inna <inna-portnoi@mail.ru>
2022-06-25 09:11:16 +0300

    Task1_1

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,2 +1,16 @@
-﻿    
-Console.WriteLine("Hello, pipl!");
+﻿string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (b== a * a)
+{
+    Console.WriteLine("Yes");
+
+}
+else
+{
+    Console.WriteLine("No");
+
+}
